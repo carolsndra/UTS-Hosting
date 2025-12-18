eaec921072e1799728938db26318de48d8f0daba
carolsndra <olinechen9@gmail.com>
2025-12-18 13:47:08 +0700

    Update history.js

--- dist/history.js
+++ dist/history.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:3000';
+const API = window.location.origin; 
 
 const historyBody = document.getElementById('historyBody');
 // Filter buttons (History only uses All/In/Out)
